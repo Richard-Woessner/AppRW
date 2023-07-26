b751371c078be6c0f90ee770343327b7ef172bb7
ricky <rick@sevenhillstechnology.com>
2023-07-26 16:22:35 -0400

    remove npm error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,18 @@
+import { View, StyleSheet, Text } from 'react-native';
 import { Home } from './home';
 
 export default function RootLayout() {
   return (
-    <>
-      <Home />
-    </>
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- app/home/index.tsx
+++ app/home/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from 'react-native';
 
 export const Home = () => {
   return (
-    <View>
+    <>
       <Text>Home</Text>
-    </View>
+    </>
   );
 };
